2ff07f04149592b8d5f7026a279223a7e7a7a8f7
Jordan Clark <jordan.g.clark@gmail.com>
2017-07-08 11:40:14 -0700

    recommit

--- app.js
+++
@@ -1,3 +0,0 @@
-// app.js
-require('babel-core/register')
-require('./app-server.js')
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-// Config
-module.exports = (app, config, partials) => {
-  require('./blog')(app, config, partials)
-  require('./404')(app, config, partials)
-}
